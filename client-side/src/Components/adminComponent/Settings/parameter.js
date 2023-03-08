f6f2570f789ed27dabd102b091400906e55243b1
palashmunshi <palashmunshi4@gmail.com>
2023-03-08 13:48:30 +0530

    routes for API calls changed to 127.0.0.1:4000

--- client-side/src/Components/adminComponent/Settings/parameter.js
+++ client-side/src/Components/adminComponent/Settings/parameter.js
@@ -1,7 +1,7 @@
-import React, {useEffect,useState} from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../../../Shared/Navbar/navbar";
 import Sidebar from "../../../Shared/Sidebar/adminSidebar";
-import '../admin.scss'
+import "../admin.scss";
 import "./table.scss";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -11,26 +11,25 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 
-
 export const Parameter = () => {
-    const [parameter, setParameter] = useState([]);
-    // const [limit, setLimit] = useState("")
-    // const [outTime, setOutTime] = useState("")
-    // const [inTime, setInTime] = useState("")
-    // const [arrivalUB, setArrivalUB] = useState("")
-    // const [arrivalLB, setArrivalLB] = useState("")
-    // const [flex, setFlex] = useState("")
+  const [parameter, setParameter] = useState([]);
+  // const [limit, setLimit] = useState("")
+  // const [outTime, setOutTime] = useState("")
+  // const [inTime, setInTime] = useState("")
+  // const [arrivalUB, setArrivalUB] = useState("")
+  // const [arrivalLB, setArrivalLB] = useState("")
+  // const [flex, setFlex] = useState("")
 
-    useEffect(() => {
-        fetch("http://192.168.9.230:4000/gatepass/v2/admin/parameter_config")
-          .then((response) => {
-            return response.json();
-          })
-          .then((text) => {
-            setParameter(text);
-          });
-          console.log(parameter)
-    })  
+  useEffect(() => {
+    fetch("http://127.0.0.1:4000/gatepass/v2/admin/parameter_config")
+      .then((response) => {
+        return response.json();
+      })
+      .then((text) => {
+        setParameter(text);
+      });
+    console.log(parameter);
+  });
 
   return (
     <div className="admin">
@@ -38,47 +37,51 @@ export const Parameter = () => {
       <div className="adminContainer">
         <Navbar />
         <div className="listContainer">
-            <div className="listTitle">Parameter Config</div>
-            <TableContainer component={Paper} className="table">
-                <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell className="tableCell">S.No.</TableCell>
-                            <TableCell className="tableCell">Parameters</TableCell>
-                            <TableCell className="tableCell">Current Value</TableCell>
-                            <TableCell className="tableCell">Change To</TableCell>
-                            <TableCell className="tableCell"></TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {parameter.map((row) => (
-                            <TableRow key={row.param_id}>
-                            <TableCell className="tableCell">{row.param_id}</TableCell>
-                            <TableCell className="tableCell">{row.parameter}</TableCell>
-                            <TableCell className="tableCell">{row.value}</TableCell>
-                            <TableCell className="tableCell">
-                                <input
-                                    className="e-input"
-                                    //value={name}
-                                    //onChange={(e) => setName(e.target.value)}
-                                    type="text"
-                                    placeholder=""
-                                />
-                            </TableCell>
-                            <TableCell className="tableCell">
-                                <button
-                                type="button" 
-                                style={{ background: "green", color: "#fff", borderRadius: "5px" }}
-                                >
-                                    Change
-                                </button>
-                            </TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        </div> 
+          <div className="listTitle">Parameter Config</div>
+          <TableContainer component={Paper} className="table">
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell className="tableCell">S.No.</TableCell>
+                  <TableCell className="tableCell">Parameters</TableCell>
+                  <TableCell className="tableCell">Current Value</TableCell>
+                  <TableCell className="tableCell">Change To</TableCell>
+                  <TableCell className="tableCell"></TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {parameter.map((row) => (
+                  <TableRow key={row.param_id}>
+                    <TableCell className="tableCell">{row.param_id}</TableCell>
+                    <TableCell className="tableCell">{row.parameter}</TableCell>
+                    <TableCell className="tableCell">{row.value}</TableCell>
+                    <TableCell className="tableCell">
+                      <input
+                        className="e-input"
+                        //value={name}
+                        //onChange={(e) => setName(e.target.value)}
+                        type="text"
+                        placeholder=""
+                      />
+                    </TableCell>
+                    <TableCell className="tableCell">
+                      <button
+                        type="button"
+                        style={{
+                          background: "green",
+                          color: "#fff",
+                          borderRadius: "5px",
+                        }}
+                      >
+                        Change
+                      </button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </div>
       </div>
     </div>
   );
