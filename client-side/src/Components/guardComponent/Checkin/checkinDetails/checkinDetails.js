102ad62aad0cdaa4a96b7dc4b08b481e9c9e9573
ayushSinha16 <ayush16.sinha@gmail.com>
2023-03-12 02:16:44 +0530

    Made the check-in page for the guard along with a few changes in the Guard Queries

--- client-side/src/Components/guardComponent/Checkin/checkinDetails/checkinDetails.js
+++ client-side/src/Components/guardComponent/Checkin/checkinDetails/checkinDetails.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./checkinDetails.scss";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -9,6 +9,23 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 
 export const CheckinDetails = () => {
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    
+      fetch(
+        "http://127.0.0.1:4000/gatepass/v2/guard/checked_out_students"
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          setUser(data);
+        })
+        .catch((err) => console.log("error:", err));
+    }, []
+  );
+
   return (
     <div className="listContainer">
       <div className="listTitle">Check-In Dashboard</div>
@@ -16,14 +33,29 @@ export const CheckinDetails = () => {
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell className="tableCell">S.No.</TableCell>
-              <TableCell className="tableCell">Enrollment</TableCell>
-              <TableCell className="tableCell">Out Time</TableCell>
-              <TableCell className="tableCell">Check In</TableCell>
-
-              <TableCell className="tableCell"></TableCell>
+              <TableCell className="tableCell">User Details</TableCell>
+              <TableCell className="tableCell">Contact</TableCell>
+              <TableCell className="tableCell">Gatepass Type</TableCell>
+              <TableCell className="tableCell">Actual Departure</TableCell>
+              <TableCell className="tableCell">Expected Arrival</TableCell>
+              <TableCell className="tableCell">Status</TableCell>
+              <TableCell className="tableCell">Action</TableCell>
             </TableRow>
           </TableHead>
+
+          <TableBody>
+          {user.map((props) => (
+              <TableRow key={props.user_id}>
+                <TableCell className="tableCell">{props.name} <br/> {props.user_id} </TableCell>
+                <TableCell className="tableCell">{props.contact_number}</TableCell>
+                <TableCell className="tableCell">{props.gatepass_name}</TableCell>
+                <TableCell className="tableCell">{new Date(props.from_date).toLocaleDateString()} <br/> {new Date(props.from_time).toLocaleTimeString()}</TableCell>
+                <TableCell className="tableCell">{new Date(props.to_date).toLocaleDateString()} <br/> {new Date(props.to_time).toLocaleTimeString()}</TableCell>
+                <TableCell className="tableCell">{props.status}</TableCell>
+                <TableCell className="tableCell"><button>Check In</button></TableCell>
+                </TableRow>
+                ))}
+          </TableBody>
         </Table>
       </TableContainer>
     </div>
