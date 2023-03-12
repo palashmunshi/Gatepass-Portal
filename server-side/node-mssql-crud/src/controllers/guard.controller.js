c9d4780f176e62088b08180654f8081fe872b8cd
palashmunshi <palashmunshi4@gmail.com>
2023-03-12 16:13:47 +0530

    added API for checkin and checkout (in progress)

--- server-side/node-mssql-crud/src/controllers/guard.controller.js
+++ server-side/node-mssql-crud/src/controllers/guard.controller.js
@@ -1,34 +1,94 @@
-import { getConnection, sql,queries } from '../database';
-const date = require('date-and-time');
-const xl = require('excel4node');
+import { getConnection, sql, queries } from "../database";
+const date = require("date-and-time");
+const xl = require("excel4node");
+
+export const getAllApproved = async (req, res) => {
+  try {
+    const pool = await getConnection();
+    const result = await pool.request().query(queries.getApprovedStudents);
+    return res.json(result.recordset);
+  } catch (error) {
+    res.status(500);
+    res.send(error.message);
+  }
+};
+
+export const getAllCheckedOut = async (req, res) => {
+  try {
+    const pool = await getConnection();
+    const result = await pool.request().query(queries.getCheckedOutStudents);
+    return res.json(result.recordset);
+  } catch (error) {
+    res.status(500);
+    res.send(error.message);
+  }
+};
 
 
+// DO NOT USE THE BELOW 2 FUNCTIONS.... THEY ARE NOT WORKING RIGHT NOW
+export const studentCheckin = async (req, res) => {
+  const { check_in_by, user_id } = req.body;
+  const currentDate = new Date();
+  let actual_in_date = currentDate.getFullYear() + "-" + currentDate.getMonth() + "-" + currentDate.getDate();
+  let actual_in_time = currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds();
 
-export const getAllApproved = async (req,res) => {
-    try{
-        const pool = await getConnection();
-        const result = await pool
-            .request()
-            .query(queries.getApprovedStudents);
-        return res.json(result.recordset);
-    }
-    catch (error){
-        res.status(500);
-        res.send(error.message)
-    }
-}
+  if (check_in_by == null) {
+    return res.status(400).json({ msg: "Bad Request" });
+  }
 
+  try {
+    const pool = await getConnection();
+    const result = await pool
+      .request()
+      .input("user_id", sql.VarChar, user_id)
+      .input("check_in_by", sql.VarChar, check_in_by)
+      .input("actual_in_date", sql.VarChar, actual_in_date)
+      .input("actual_in_time", sql.VarChar, actual_in_time)
+      .query(queries.studentCheckin);
 
-export const getAllCheckedOut = async (req,res) => {
-    try{
-        const pool = await getConnection();
-        const result = await pool
-        .request()
-        .query(queries.getCheckedOutStudents);
-        return res.json(result.recordset);
-    }
-    catch(error){
-        res.status(500);
-        res.send(error.message)
-    }
-}
+    return res.send("Student in now Checkedin");
+  } catch (error) {
+    res.status(500);
+    res.send(error.message);
+  }
+};
+
+export const studentCheckout = async (req, res) => {
+  const { check_out_by, user_id } = req.body;
+  const currentDate = new Date();
+  let actual_out_date =
+    currentDate.getFullYear() +
+    "-" +
+    currentDate.getMonth() +
+    "-" +
+    currentDate.getDate();
+  let actual_out_time =
+    currentDate.getHours() +
+    ":" +
+    currentDate.getMinutes() +
+    ":" +
+    currentDate.getSeconds();
+
+  if (check_out_by == null) {
+    return res.status(400).json({ msg: "Bad Request" });
+  }
+
+  try {
+    const pool = await getConnection();
+    const result = await pool
+      .request()
+      .input("user_id", sql.VarChar, user_id)
+      .input("check_out_by", sql.VarChar, check_out_by)
+      .input("actual_out_date", sql.VarChar, actual_out_date)
+      .input("actual_out_time", sql.VarChar, actual_out_time)
+      .query(queries.studentCheckout);
+
+    return res.send("Student is now Checkedout");
+  } catch (error) {
+    res.status(500);
+    res.send(error.message);
+  }
+};
+
+// YOU CAN START WRITING CODE FROM HERE
+//
--- server-side/node-mssql-crud/src/routes/guard.routes.js
+++ server-side/node-mssql-crud/src/routes/guard.routes.js
@@ -1,16 +1,20 @@
 import { Router } from "express";
 import {
-    getAllApproved,
-    getAllCheckedOut,
+  getAllApproved,
+  getAllCheckedOut,
+  studentCheckin,
+  studentCheckout,
 } from "../controllers/guard.controller";
 
 const router = Router();
 
 /* __________________________________________________CHECK OUT ROUTES__________________________________________________ */
 
-router.get('/guard/approved_students',getAllApproved);
+router.get("/guard/approved_students", getAllApproved);
+router.put("/guard/checkout_student", studentCheckout); //DO NOT USE THIS ROUTE RIGHT NOW
 
 /* __________________________________________________CHECK IN ROUTES__________________________________________________ */
-router.get('/guard/checked_out_students',getAllCheckedOut);
+router.get("/guard/checked_out_students", getAllCheckedOut);
+router.put("/guard/checkin_student", studentCheckin); //DO NOT USE THIS ROUTE RIGHT NOW
 
 export default router;
