402f58720d4063523c53b4abfbba15fc033183e7
ayushSinha16 <ayush16.sinha@gmail.com>
2023-03-14 15:08:13 +0530

    Fixed the date and time in the checkout page of the guard component

--- client-side/src/Components/guardComponent/Checkout/checkoutDetails/checkoutDetails.js
+++ client-side/src/Components/guardComponent/Checkout/checkoutDetails/checkoutDetails.js
@@ -7,7 +7,7 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { TableBody } from "@mui/material";
-
+import moment from "moment";
 
 export const CheckoutDetails = () => {
   const [user, setUser] = useState([]);
@@ -16,7 +16,8 @@ export const CheckoutDetails = () => {
     fetch("http://localhost:4000/gatepass/v2/guard/approved_students")
       .then((response) => {
         return response.json();
-      }).then((text) => {
+      })
+      .then((text) => {
         setUser(text);
       });
   }, []);
@@ -32,26 +33,54 @@ export const CheckoutDetails = () => {
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
             <TableRow>
-            <TableCell className="tableCell" id="title">User Details</TableCell>
-              <TableCell className="tableCell" id="title">Contact</TableCell>
-              <TableCell className="tableCell" id="title">Gatepass Type</TableCell>
-              <TableCell className="tableCell" id="title">Actual Departure</TableCell>
-              <TableCell className="tableCell" id="title">Expected Arrival</TableCell>
-              <TableCell className="tableCell" id="title">Status</TableCell>
-              <TableCell className="tableCell" id="title">Action</TableCell>
+              <TableCell className="tableCell" id="title">
+                User Details
+              </TableCell>
+              <TableCell className="tableCell" id="title">
+                Contact
+              </TableCell>
+              <TableCell className="tableCell" id="title">
+                Gatepass Type
+              </TableCell>
+              <TableCell className="tableCell" id="title">
+                Departure Time
+              </TableCell>
+              <TableCell className="tableCell" id="title">
+                Expected Arrival
+              </TableCell>
+              <TableCell className="tableCell" id="title">
+                Status
+              </TableCell>
+              <TableCell className="tableCell" id="title">
+                Action
+              </TableCell>
             </TableRow>
           </TableHead>
 
           <TableBody>
             {user.map((props) => (
               <TableRow key={props.user_id}>
-                <TableCell className="tableCell">{props.name} <br/> {props.user_id}</TableCell>
-                <TableCell className="tableCell">{props.contact_number}</TableCell>
-                <TableCell className="tableCell">{props.gatepass_name}</TableCell>
-                <TableCell className="tableCell">{new Date(props.from_date).toLocaleDateString()} <br/> {new Date(props.from_time).toLocaleTimeString()}</TableCell>
-                <TableCell className="tableCell">{new Date(props.to_date).toLocaleDateString()} <br/> {new Date(props.to_time).toLocaleTimeString()}</TableCell>
+                <TableCell className="tableCell">
+                  {props.name} <br /> {props.user_id}
+                </TableCell>
+                <TableCell className="tableCell">
+                  {props.contact_number}
+                </TableCell>
+                <TableCell className="tableCell">
+                  {props.gatepass_name}
+                </TableCell>
+                <TableCell className="tableCell">
+                  {moment(props.from_date).utc().format("YYYY-MM-DD")} <br />{" "}
+                  {moment(props.from_time).utc().format("HH:mm:ss")}
+                </TableCell>
+                <TableCell className="tableCell">
+                  {moment(props.to_date).utc().format("YYYY-MM-DD")} <br />{" "}
+                  {moment(props.to_time).utc().format("HH:mm:ss")}
+                </TableCell>
                 <TableCell className="tableCell">{props.status}</TableCell>
-                <TableCell className="tableCell"><button id="button1">Check Out</button></TableCell>
+                <TableCell className="tableCell">
+                  <button id="button1">Check Out</button>
+                </TableCell>
               </TableRow>
             ))}
           </TableBody>
