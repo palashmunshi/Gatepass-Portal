bae2c1706ed8a60f8a260f2c5b17397371bb2390
Kshitiz Sinha <kshitiz.sinha.025@gmail.com>
2023-03-19 03:27:50 +0530

    done with the group part

--- client-side/src/Components/adminComponent/Settings/group.js
+++ client-side/src/Components/adminComponent/Settings/group.js
@@ -1,18 +1,14 @@
-
 import React, { useState, useEffect } from "react";
 import Navbar from "../../../Shared/Navbar/navbar";
 import Sidebar from "../../../Shared/Sidebar/adminSidebar";
 import "./group.scss";
 
-
 export const Group = () => {
   const [groups, setGroups] = useState([]);
   const [subgroups, setSubgroups] = useState([]);
-  const counter = () =>{
-    let counter1 = 0;
-    counter1++;
-    return counter1;
-  }
+  const [showMore, setShowMore] = useState(false);
+  const [showMore1, setShowMore1] = useState(false);
+  const [showPopup, setShowPopup] = useState(false);
 
   useEffect(() => {
     fetch("http://localhost:4000/gatepass/v2/admin/get_all_groups")
@@ -20,17 +16,32 @@ export const Group = () => {
       .then((data) => setGroups(data))
       .catch((error) => console.log(error));
 
-      fetch("http://localhost:4000/gatepass/v2/admin/get_all_sub_groups")
+    fetch("http://localhost:4000/gatepass/v2/admin/get_all_sub_groups")
       .then((response) => response.json())
       .then((data) => setSubgroups(data))
       .catch((error) => console.log(error));
-
-     
-
-      
   }, []);
 
+  const handleShowMore = () => {
+    setShowMore(true);
+    setShowPopup(true);
+  };
 
+  const handleShowLess = () => {
+    setShowMore(false);
+  };
+
+  const handleShowMore1 = () => {
+    setShowMore1(true);
+  };
+
+  const handleShowLess1 = () => {
+    setShowMore1(false);
+  };
+
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
 
   return (
     <div className="admin">
@@ -41,30 +52,53 @@ export const Group = () => {
           <div className="GroupDesign">
             <div className="Align">
               <div id="heading">
-                <h5>Add/Update Groups</h5>  
+                <h5>Add/Update Groups</h5>
               </div>
             </div>
             <div className="search3">
-              <input type="text" placeholder="Make a new group" id="search1"></input>
+              <input
+                type="text"
+                placeholder="Make a new group"
+                id="search1"
+              ></input>
               <button id="add">+</button>
               <table className="table1">
-            <thead>
-              <tr>
-                <th>Group ID</th>
-                <th>Group Name</th>
-                <th>Master ID</th>
-              </tr>
-            </thead>
-            <tbody>
-              {groups.map((group) => (
-                <tr key={group.gps_groupid}>
-                  <td>{group.gps_groupid}</td>
-                  <td>{group.gps_groupname}</td>
-                  <td>{group.gps_group_mastergroup_id}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+                <thead>
+                  <tr>
+                    <th>S.No</th>
+                    <th>Group Name</th>
+                    <th>Change</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {groups
+                    .slice(0, showMore ? groups.length : 10)
+                    .map((group, index) => (
+                      <tr key={group.gps_groupid}>
+                        <td>{index + 1}</td>
+                        <td>{group.gps_groupname}</td>
+                        <td>
+                          <button
+                            className="modify"
+                            onClick={() => alert("Under Construction!")}
+                          >
+                            Modify
+                          </button>
+                        </td>
+                      </tr>
+                    ))}
+                </tbody>
+              </table>
+              {groups.length > 10 && !showMore && (
+                <p onClick={handleShowMore} className="eventShow">
+                  Show More
+                </p>
+              )}
+              {showMore && (
+                <p onClick={handleShowLess} className="eventShow">
+                  Show Less
+                </p>
+              )}
             </div>
           </div>
           <div className="GroupDesign">
@@ -74,31 +108,52 @@ export const Group = () => {
               </div>
             </div>
             <div className="search3">
-              <input type="text" placeholder="Make a new subgroup" id="search1"></input>
+              <input
+                type="text"
+                placeholder="Make a new subgroup"
+                id="search1"
+              ></input>
               <button id="add">+</button>
               <table className="table1">
                 <thead>
                   <tr>
                     <th>S.No</th>
                     <th>Subgroup Name</th>
-                    <th>Edit/Update</th>
+                    <th>Change</th>
                   </tr>
                 </thead>
                 <tbody>
-                  {subgroups.map((subgroup) => (
-                    <tr key={subgroup.subgroup_id}>
-                      <td>{counter}</td>
-                      <td>{subgroup.subgroup_name}</td>
-                      <td><button>Update</button></td>
-                    </tr>
-                  ))}
+                  {subgroups
+                    .slice(0, showMore1 ? subgroups.length : 10)
+                    .map((subgroup, index) => (
+                      <tr key={subgroups.gps_groupid}>
+                        <td>{index + 1}</td>
+                        <td>{subgroup.subgroup_name}</td>
+                        <td>
+                          <button
+                            className="modify"
+                            onClick={() => alert("To be done!")}
+                          >
+                            Modify
+                          </button>
+                        </td>
+                      </tr>
+                    ))}
                 </tbody>
               </table>
+              {subgroups.length > 10 && !showMore1 && (
+                <p onClick={handleShowMore1} className="eventShow">
+                  Show More
+                </p>
+              )}
+              {showMore1 && (
+                <p onClick={handleShowLess1} className="eventShow">
+                  Show Less
+                </p>
+              )}
             </div>
           </div>
-         
         </div>
-        <p>Note- The table is fucked</p>
       </div>
     </div>
   );
