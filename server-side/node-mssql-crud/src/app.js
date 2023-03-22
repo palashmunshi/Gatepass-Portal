97350d40922efd38a13ac06163d6eb3e2e4aa621
palashmunshi <75259660+palashmunshi@users.noreply.github.com>
2023-03-22 12:36:02 +0530

    Auth api made (#47)
    * query added
    
    * added the contoller function
    
    * routes made for the auth API
    
    * routes added in the app
    
    * header changed

--- server-side/node-mssql-crud/src/app.js
+++ server-side/node-mssql-crud/src/app.js
@@ -5,6 +5,7 @@ import adminRoutes from "./routes/admin.routes";
 import studentRoutes from "./routes/student.routes";
 import wardenRoutes from "./routes/warden.routes";
 import guardRoutes from "./routes/guard.routes";
+import authRoutes from "./routes/auth.routes";
 import morgan from "morgan";
 import cors from "cors";
 
@@ -24,5 +25,6 @@ app.use("/gatepass/v2", adminRoutes);
 app.use("/gatepass/v2", studentRoutes);
 app.use("/gatepass/v2", wardenRoutes);
 app.use("/gatepass/v2", guardRoutes);
+app.use("/gatepass/v2", authRoutes);
 
 export default app;
