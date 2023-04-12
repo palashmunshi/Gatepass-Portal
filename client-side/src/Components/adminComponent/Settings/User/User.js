e9fa02554f811d72ed02dce8b401db6623b352fe
ayushSinha16 <ayush16.sinha@gmail.com>
2023-04-12 22:00:24 +0545

    Details being fetched and added into modal

--- client-side/src/Components/adminComponent/Settings/User/User.js
+++ client-side/src/Components/adminComponent/Settings/User/User.js
@@ -15,6 +15,7 @@ export const User = () => {
   const [search, setSearch] = useState("");
   const [filterData, setFilterData] = useState([]);
   const [modal, setModal] = useState(false);
+  const [details, setDetails] = useState([])
 
   useEffect(() => {
     const fetchData = async () => {
@@ -53,13 +54,11 @@ export const User = () => {
     }
   };
 
-  const fillModal = (event) => {
-    const user_id = event.target.value;
-    console.log(event.target)
-  }
-
-  const toggleModal = () => {
-    setModal(!modal);
+  const fillModal = async (event) => {
+    const user_id = event.target.name;
+    await fetch(
+      `http://localhost:4000/gatepass/v2/admin/user_info/${user_id}`
+    ).then((res) => res.json()).then((data) => setDetails(data));
   };
 
   return (
@@ -83,118 +82,130 @@ export const User = () => {
             onRequestClose={() => setModal(false)}
           // style={(className = "modalContainer")}
           >
-            <div class="title">Edit User</div>
 
-            <form>
-              <div class="container">
-                <div class="column left-column">
-                  <label for="name" id="name-label">
-                    Name:
-                  </label>
-                  <input
-                    type="text"
-                    id="name"
-                    name="name"
-                    aria-labelledby="name-label"
-                  ></input>
-                  <label for="user_id" id="user-id-label">
-                    Enrollment Number:
-                  </label>
-                  <input
-                    type="text"
-                    id="user_id"
-                    name="user_id"
-                    aria-labelledby="user-id-label"
-                  ></input>
-                  <label for="email" id="email-label">
-                    Email ID:
-                  </label>
-                  <input
-                    type="text"
-                    id="email"
-                    name="email"
-                    aria-labelledby="email-label"
-                  ></input>
-                  <label for="role" id="role-label">
-                    Role:
-                  </label>
-                  <select id="role" name="role" aria-labelledby="role-label">
-                    <option value="">-Role-</option>
-                    <option value="student">Student</option>
-                    <option value="faculty">Faculty</option>
-                    <option value="staff">Staff</option>
-                  </select>
+            <div class="title">Edit User</div>
+            {details.slice(0).map((user) => (
+              <form>
+                <div class="container">
+                  <div class="column left-column">
+                    <label for="name" id="name-label">
+                      Name:
+                    </label>
+                    <input
+                      type="text"
+                      id="name"
+                      name="name"
+                      aria-labelledby="name-label"
+                      defaultValue={user.name}
+                      disabled={true}
+                    ></input>
+                    <label for="user_id" id="user-id-label">
+                      Enrollment Number:
+                    </label>
+                    <input
+                      type="text"
+                      id="user_id"
+                      name="user_id"
+                      aria-labelledby="user-id-label"
+                      defaultValue={user.user_id}
+                      disabled={true}
+                    ></input>
+                    <label for="email" id="email-label">
+                      Email ID:
+                    </label>
+                    <input
+                      type="text"
+                      id="email"
+                      name="email"
+                      aria-labelledby="email-label"
+                      defaultValue={user.email_id}
+                      disabled={true}
+                    ></input>
+                    <label for="role" id="role-label">
+                      Role:
+                    </label>
+                    <select id="role" name="role" aria-labelledby="role-label">
+                      <option value="">-Role-</option>
+                      <option value="student">Student</option>
+                      <option value="faculty">Faculty</option>
+                      <option value="staff">Staff</option>
+                    </select>
+                  </div>
+                  <div class="column right-column">
+                    <label for="group_setting" id="group-setting-label">
+                      Group Setting:
+                    </label>
+                    <select
+                      id="group_setting"
+                      name="group_setting"
+                      aria-labelledby="group-setting-label"
+                    >
+                      <option value="no_role">No Role</option>
+                    </select>
+                    <label for="subgroup_setting" id="subgroup-setting-label">
+                      Subgroup Setting:
+                    </label>
+                    <select
+                      id="subgroup_setting"
+                      name="subgroup_setting"
+                      aria-labelledby="subgroup-setting-label"
+                    >
+                      <option value="no_role">No Role</option>
+                    </select>
+                    <label for="hostel" id="hostel-label">
+                      Hostel:
+                    </label>
+                    <input
+                      type="text"
+                      id="hostel"
+                      name="hostel"
+                      aria-labelledby="hostel-label"
+                      defaultValue={user.hostel}
+                    ></input>
+                    <label for="room_number" id="room-number-label">
+                      Room Number:
+                    </label>
+                    <input
+                      type="text"
+                      id="room_number"
+                      name="room_number"
+                      aria-labelledby="room-number-label"
+                      defaultValue={user.room_no}
+                    ></input>
+                    <label for="contact_number" id="contact-number-label">
+                      Contact Number:
+                    </label>
+                    <input
+                      type="text"
+                      id="contact_number"
+                      name="contact_number"
+                      aria-labelledby="contact-number-label"
+                      defaultValue={user.contact_number}
+                    ></input>
+                    <label
+                      for="parents_contact_number"
+                      id="parents-contact-number-label"
+                    >
+                      Parents Contact Number:
+                    </label>
+                    <input
+                      type="text"
+                      id="parents_contact_number"
+                      name="parents_contact_number"
+                      aria-labelledby="parents-contact-number-label"
+                      defaultValue={user.p_number}
+                    ></input>
+                  </div>
                 </div>
-                <div class="column right-column">
-                  <label for="group_setting" id="group-setting-label">
-                    Group Setting:
-                  </label>
-                  <select
-                    id="group_setting"
-                    name="group_setting"
-                    aria-labelledby="group-setting-label"
-                  >
-                    <option value="no_role">No Role</option>
-                  </select>
-                  <label for="subgroup_setting" id="subgroup-setting-label">
-                    Subgroup Setting:
-                  </label>
-                  <select
-                    id="subgroup_setting"
-                    name="subgroup_setting"
-                    aria-labelledby="subgroup-setting-label"
-                  >
-                    <option value="no_role">No Role</option>
-                  </select>
-                  <label for="hostel" id="hostel-label">
-                    Hostel:
-                  </label>
-                  <input
-                    type="text"
-                    id="hostel"
-                    name="hostel"
-                    aria-labelledby="hostel-label"
-                  ></input>
-                  <label for="room_number" id="room-number-label">
-                    Room Number:
-                  </label>
-                  <input
-                    type="text"
-                    id="room_number"
-                    name="room_number"
-                    aria-labelledby="room-number-label"
-                  ></input>
-                  <label for="contact_number" id="contact-number-label">
-                    Contact Number:
-                  </label>
-                  <input
-                    type="text"
-                    id="contact_number"
-                    name="contact_number"
-                    aria-labelledby="contact-number-label"
-                  ></input>
-                  <label
-                    for="parents_contact_number"
-                    id="parents-contact-number-label"
-                  >
-                    Parents Contact Number:
-                  </label>
-                  <input
-                    type="text"
-                    id="parents_contact_number"
-                    name="parents_contact_number"
-                    aria-labelledby="parents-contact-number-label"
-                  ></input>
-                </div>
-              </div>
-              <button
-                type="submit"
-                id="update-button"
-                onClick={() => setModal(false)}
-              >
-                Update User
-              </button>
-            </form>
+                <button
+                  type="submit"
+                  id="update-button"
+                  onClick={() => setModal(false)}
+                >
+                  Update User
+                </button>
+              </form>
+            ))}
           </ReactModal>
           <div className="ContentShaper">
             <div className="TableShaper">
@@ -204,23 +215,23 @@ export const User = () => {
                     <th>S.No</th>
 
                     <th>Name</th>
-                    <th>Status</th>
+                    <th>Action</th>
                   </tr>
                 </thead>
                 <tbody>
                   {search.length > 1
                     ? filterData.slice(0, displayCount).map((user, index) => (
-                      <tr key={user.user_id} onClick={fillModal}>
+                      <tr key={user.user_id} onClick={() => setModal(true)}>
                         <td>{index + 1}</td>
                         <td>{user.name}</td>
-                        <td>{user.status}</td>
+                        <td><button onClick={fillModal} name={user.user_id}>Edit</button></td>
                       </tr>
                     ))
                     : data.slice(0, displayCount).map((user, index) => (
                       <tr key={user.user_id} onClick={() => setModal(true)}>
                         <td>{index + 1}</td>
                         <td>{user.name}</td>
-                        <td>{user.status}</td>
+                        <td><button onClick={fillModal} name={user.user_id}>Edit</button></td>
                       </tr>
                     ))}
                 </tbody>
