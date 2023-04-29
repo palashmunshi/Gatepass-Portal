03e113e92fdfa2a77303c4e30ada42529aadbff4
palashmunshi <palashmunshi4@gmail.com>
2023-04-30 00:52:34 +0530

    middleware for authentication

---
+++ server-side/node-mssql-crud/src/middleware/authenticate.js
@@ -0,0 +1,14 @@
+import { jsonwebtoken } from "jsonwebtoken";
+
+export const authenticate = (req, res, next) => {
+  const access_token = req.headers.authorization;
+  const jwt = require("jsonwebtoken");
+  jwt.verify(access_token, "secret", function (err, decoded) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Token Verified");
+      next();
+    }
+  });
+};
