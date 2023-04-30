326964439e36096c39f981c7b9da9772676faf8b
palashmunshi <palashmunshi4@gmail.com>
2023-04-30 11:14:55 +0530

    removed console log statements

--- server-side/node-mssql-crud/src/middleware/authenticate.js
+++ server-side/node-mssql-crud/src/middleware/authenticate.js
@@ -5,10 +5,8 @@ export const authenticate = (req, res, next) => {
   const jwt = require("jsonwebtoken");
   jwt.verify(access_token, "secret", function (err, decoded) {
     if (err) {
-      console.log(err);
       res.json({ error: err });
     } else {
-      console.log("Token Verified");
       next();
     }
   });
