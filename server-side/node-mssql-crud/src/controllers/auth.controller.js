d6bbc88a072462436f649d2fa325aa1269340541
palashmunshi <palashmunshi4@gmail.com>
2023-04-30 10:44:13 +0530

    removed the API

--- server-side/node-mssql-crud/src/controllers/auth.controller.js
+++ server-side/node-mssql-crud/src/controllers/auth.controller.js
@@ -3,21 +3,6 @@ import jwt_decode from "jwt-decode";
 import jwt_encode from "jwt-encode";
 import jsonwebtoken from "jsonwebtoken";
 
-export const getUserInformation = async (req, res) => {
-  try {
-    const { email_id } = req.params;
-    const pool = await getConnection();
-    const result = await pool
-      .request()
-      .input("email_id", sql.VarChar, email_id)
-      .query(queries.getUserInformation);
-    return res.json(result.recordset[0]);
-  } catch (error) {
-    res.status(500);
-    res.send(error.message);
-  }
-};
-
 export const JWTgeneration = async (req, res) => {
   const { googleJWT } = req.body;
   const userObject = jwt_decode(googleJWT);
--- server-side/node-mssql-crud/src/routes/auth.routes.js
+++ server-side/node-mssql-crud/src/routes/auth.routes.js
@@ -1,8 +1,7 @@
 import { Router } from "express";
-import { JWTgeneration, getUserInformation } from "../controllers/auth.controller";
+import { JWTgeneration } from "../controllers/auth.controller";
 
 const router = Router();
 
-router.get("/auth/user_information/:email_id", getUserInformation);
-router.post("/auth/google_JWT", JWTgeneration)
+router.post("/auth/google_JWT", JWTgeneration);
 export default router;
