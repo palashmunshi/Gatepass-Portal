a7480dc864c880e2db38d59e826a8b9f8e07c514
palashmunshi <palashmunshi4@gmail.com>
2023-04-30 12:57:50 +0530

    middleware will check the blackliste jwt table

--- server-side/node-mssql-crud/src/middleware/authenticate.js
+++ server-side/node-mssql-crud/src/middleware/authenticate.js
@@ -1,13 +1,25 @@
 import { jsonwebtoken } from "jsonwebtoken";
+import { getConnection, sql, queries } from "../database";
 
-export const authenticate = (req, res, next) => {
+export const authenticate = async (req, res, next) => {
   const access_token = req.headers.authorization;
   const jwt = require("jsonwebtoken");
-  jwt.verify(access_token, "secret", function (err, decoded) {
-    if (err) {
-      res.json({ error: err });
-    } else {
-      next();
-    }
-  });
+
+  const pool = await getConnection();
+  const result = await pool
+    .request()
+    .input("jwt", sql.VarChar, access_token)
+    .query(queries.getBlacklistedJwt);
+  const jwtCount = result.recordset[0]["TOTAL"];
+  if (jwtCount === 0) {
+    jwt.verify(access_token, "secret", function (err, decoded) {
+      if (err) {
+        res.json({ error: err });
+      } else {
+        next();
+      }
+    });
+  } else {
+    res.json({ ACCESS_DENIED: "JWT has expired" });
+  }
 };
