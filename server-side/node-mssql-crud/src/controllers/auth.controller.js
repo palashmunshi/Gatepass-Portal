9a5581ceb8de7d09a97663b5bc2222cede49dcec
palashmunshi <palashmunshi4@gmail.com>
2023-04-30 11:11:58 +0530

    google jwt verification

--- server-side/node-mssql-crud/src/controllers/auth.controller.js
+++ server-side/node-mssql-crud/src/controllers/auth.controller.js
@@ -2,9 +2,24 @@ import { getConnection, sql, queries } from "../database";
 import jwt_decode from "jwt-decode";
 import jwt_encode from "jwt-encode";
 import jsonwebtoken from "jsonwebtoken";
+import { OAuth2Client } from "google-auth-library";
 
 export const JWTgeneration = async (req, res) => {
+  const client = new OAuth2Client(
+    "372946592599-u1gj83quodhpdae46ejslj4tto3mn3vn.apps.googleusercontent.com"
+  );
   const { googleJWT } = req.body;
+
+  const verify = async () => {
+    const ticket = await client.verifyIdToken({
+      idToken: googleJWT,
+      audience:
+        "372946592599-u1gj83quodhpdae46ejslj4tto3mn3vn.apps.googleusercontent.com",
+    });
+    const payload = ticket.getPayload();
+  };
+  verify().catch(console.error);
+
   const userObject = jwt_decode(googleJWT);
   const email_id = userObject.email;
   const pool = await getConnection();
