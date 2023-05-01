b250bba6c3926780d9086d3d609bd5713b9516d1
ayushSinha16 <ayush16.sinha@gmail.com>
2023-05-01 22:41:06 +0530

    Route made and tested

--- server-side/node-mssql-crud/src/routes/warden.routes.js
+++ server-side/node-mssql-crud/src/routes/warden.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { 
-    gatepassApproveOrReject, } from "../controllers/warden.controller"
+    gatepassApproveOrReject, gatepassCancelAndReject, } from "../controllers/warden.controller"
 
 const router = Router();
 
 router.get('/warden/gatepass_approve_or_reject/:id/:flag', gatepassApproveOrReject);
+router.get('/warden/cancelled_and_rejected_gatepass', gatepassCancelAndReject);
 
 
 export default router;
