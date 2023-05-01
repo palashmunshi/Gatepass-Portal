9c660918adad3bdbaf4c0e21674224a1a50c5d52
Karam <Karam.arora20@st.niituniversity.in>
2023-05-01 16:00:06 +0530

    query fixed

--- server-side/node-mssql-crud/src/database/query.js
+++ server-side/node-mssql-crud/src/database/query.js
@@ -93,7 +93,7 @@ export const queries = {
 
   acceptGatepass: "UPDATE [gps_db].[gps_db].[gps_gatepassmaster] SET status='Approved' WHERE request_id=@id;",
   rejectGatepass: "UPDATE [gps_db].[gps_db].[gps_gatepassmaster] SET status='Rejected' WHERE request_id=@id;",
-  getApprovedGatepass: "SELECT name, gps_db.gps_gatepass_type.gatepass_name,approved_or_rejected_date, [gps_db].[gps_db].[gps_gatepassmaster].status FROM [gps_db].[gps_db].[gps_gatepassmaster] JOIN gps_db.gps_gatepass_type ON gps_gatepassmaster.gatepass_type = gps_gatepass_type.gatepass_type JOIN gps_db.gps_usersmaster ON gps_gatepassmaster.user_id= gps_db.gps_usersmaster.user_id WHERE send_approval_to=@user_id and ([gps_db].[gps_db].[gps_gatepassmaster].status='Approved' or [gps_db].[gps_db].[gps_gatepassmaster].status='CHECKEDIN');",
+  getApprovedGatepass: "SELECT name, gatepass_name,approved_or_rejected_date,GM.status FROM [gps_db].[gps_db].[gps_gatepassmaster] AS GM JOIN gps_db.gps_gatepass_type as GT ON GM.gatepass_type = GT.gatepass_type JOIN gps_db.gps_usersmaster as GU ON GM.user_id= GU.user_id WHERE send_approval_to= @user_id and GM.status IN ('Approved' ,'CHECKEDIN' ,'CHECKEDOUT');",
 
   /* __________________________________________________Local Fixed QUERIES__________________________________________________ */
   getNumberOfLocalFixedConfig: "SELECT value from [gps_db].[gps_db].[gps_configmaster] WHERE parameter='Week Limit'",
