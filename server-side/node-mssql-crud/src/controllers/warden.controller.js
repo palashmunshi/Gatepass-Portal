dadc3681e84a9faea2d6757abfdaecf1a0959987
Karam <Karam.arora20@st.niituniversity.in>
2023-05-02 00:47:14 +0530

    api created and tested

--- server-side/node-mssql-crud/src/controllers/warden.controller.js
+++ server-side/node-mssql-crud/src/controllers/warden.controller.js
@@ -23,3 +23,26 @@ export const gatepassApproveOrReject = async (req, res) => {
             res.send(error.message);
         }
 }
+export const cancelApprovedGatepass= async (req,res)=>{
+    try {
+        const d = new Date();
+        let applied_date = d.getFullYear() + "-" + d.getMonth() + "-" + d.getDate();
+        const id = req.user.data.user_id;
+        const {req_id,comments}= req.body;
+        console.log(req_id);
+        console.log(comments);
+        const pool = await getConnection();
+        const result = await pool
+          .request()
+          .input("user_id", sql.VarChar, id)
+          .input("req_id",sql.Int,req_id)
+          .input("comments",sql.VarChar,comments)
+          .input("date",sql.VarChar,applied_date)
+          .input("time",sql.Time,d)
+          .query(queries.cancelApprovedGatepass);
+        return res.send("Gatepass Cancelled!");
+      } catch (error) {
+        res.status(500);
+        res.send(error.message);
+      }
+    };
--- server-side/node-mssql-crud/src/routes/warden.routes.js
+++ server-side/node-mssql-crud/src/routes/warden.routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { 
-    gatepassApproveOrReject, } from "../controllers/warden.controller"
+import { gatepassApproveOrReject, cancelApprovedGatepass } from "../controllers/warden.controller"
 
 const router = Router();
 
 router.get('/warden/gatepass_approve_or_reject/:id/:flag', gatepassApproveOrReject);
+router.put('/warden/cancelApprovedGatepass', cancelApprovedGatepass) // requires gatepass req_id and comments from the client
 
 
 export default router;
