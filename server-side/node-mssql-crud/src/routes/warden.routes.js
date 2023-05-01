bcd4b1e699b0b6ecd30165c8efc20aabce68a07d
Karam <Karam.arora20@st.niituniversity.in>
2023-05-01 20:41:14 +0530

    route fixed

--- server-side/node-mssql-crud/src/routes/warden.routes.js
+++ server-side/node-mssql-crud/src/routes/warden.routes.js
@@ -5,7 +5,7 @@ import { getApprovedGatepass } from "../controllers/warden.controller"
 const router = Router();
 
 router.get('/warden/gatepass_approve_or_reject/:id/:flag', gatepassApproveOrReject);
-router.get('/warden/getApprovedGatepass/:user_id', getApprovedGatepass);
+router.get('/warden/getApprovedGatepass', getApprovedGatepass);
 
 
 export default router;
