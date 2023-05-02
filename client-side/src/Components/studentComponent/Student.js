c3ff1c327dacae41be475c4a11e21bec50ecceaa
Kshitiz Sinha <kshitiz.sinha.025@gmail.com>
2023-05-02 21:34:43 +0530

    some more changes

--- client-side/src/Components/studentComponent/Student.js
+++ client-side/src/Components/studentComponent/Student.js
@@ -2,19 +2,22 @@ import React from 'react'
 
 
 import StudentDashboard from './StudentDashboard/LatestTable/table';
+import StudentNavbar from '../../Shared/NavbarTailwind/StudentNavbar';
+import StudentSidebar from '../../Shared/SideBarTailWind/StudentSidebar';
 
 export const Student = () => {
     return (
         <div className="admin">
-          <SidebarStudent />
+         <StudentNavbar/> 
+          <StudentSidebar/>
           <div className="adminContainer">
-            <Navbar /> 
+           
             <div className="listContainer">
               <div className="listTitle">Latest Gatepasses</div>
               <StudentDashboard />          
             </div>
           </div>
           </div>
-        </div>
+        
       );
 }
