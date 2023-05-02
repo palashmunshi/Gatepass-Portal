dea9537705bebfe08fd2362df22bf4b25b2e2316
Karam <Karam.arora20@st.niituniversity.in>
2023-05-02 19:02:10 +0530

    reject-gatepass api

--- server-side/node-mssql-crud/src/controllers/warden.controller.js
+++ server-side/node-mssql-crud/src/controllers/warden.controller.js
@@ -23,24 +23,24 @@ export const gatepassApproveOrReject = async (req, res) => {
             res.send(error.message);
         }
 }
-export const cancelApprovedGatepass= async (req,res)=>{
+export const rejectGatepass= async (req,res)=>{
     try {
-        const d = new Date();
-        let applied_date = d.getFullYear() + "-" + d.getMonth() + "-" + d.getDate();
+        const currentDate = new Date();
+        let applied_date =   currentDate.getFullYear() +"-" +String(currentDate.getMonth() + 1).padStart(2, "0") +"-" +String(currentDate.getDate() + 1).padStart(2, "0")
         const id = req.user.data.user_id;
-        const {req_id,comments}= req.body;
-        console.log(req_id);
+        const {request_id,comments}= req.body;
+        console.log(request_id);
         console.log(comments);
         const pool = await getConnection();
         const result = await pool
           .request()
           .input("user_id", sql.VarChar, id)
-          .input("req_id",sql.Int,req_id)
+          .input("request_id",sql.Int,request_id)
           .input("comments",sql.VarChar,comments)
           .input("date",sql.VarChar,applied_date)
-          .input("time",sql.Time,d)
+          .input("time",sql.Time,currentDate)
           .query(queries.cancelApprovedGatepass);
-        return res.send("Gatepass Cancelled!");
+        return res.send("Gatepass rejected!");
       } catch (error) {
         res.status(500);
         res.send(error.message);
