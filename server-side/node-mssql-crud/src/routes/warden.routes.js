923031cecc9bb05c88befd87f1f84e5f7dac2db3
palashmunshi <palashmunshi4@gmail.com>
2023-05-02 15:41:10 +0530

    routes defined

--- server-side/node-mssql-crud/src/routes/warden.routes.js
+++ server-side/node-mssql-crud/src/routes/warden.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { 
+    approveGatepass,
     gatepassApproveOrReject, } from "../controllers/warden.controller"
 
 const router = Router();
 
 router.get('/warden/gatepass_approve_or_reject/:id/:flag', gatepassApproveOrReject);
+router.put('/warden/approve_gatepass', approveGatepass)
 
 
 export default router;
