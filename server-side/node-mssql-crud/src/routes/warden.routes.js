6ed6cf96f977462f30844b4addaba7f305c9f2ad
ayushSinha16 <ayush16.sinha@gmail.com>
2023-05-03 17:15:42 +0530

    Fixed route naming

--- server-side/node-mssql-crud/src/routes/warden.routes.js
+++ server-side/node-mssql-crud/src/routes/warden.routes.js
@@ -11,6 +11,6 @@ router.put("/warden/approve_gatepass", approveGatepass);
 router.put('/warden/reject_Gatepass', rejectGatepass) // requires gatepass req_id and comments from the client
 router.get('/warden/cancelled_and_rejected_gatepass', gatepassCancelAndReject);
 router.get("/warden/getApprovedGatepass", getApprovedGatepass);
-router.get("/warden/AutoApprovedBatches",getAutoApprovedBatches);
+router.get("/warden/auto_approved_batches",getAutoApprovedBatches);
 
 export default router;
