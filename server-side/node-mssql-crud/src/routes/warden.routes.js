9abde3125c1669c799ec1ffce6f96afd509e701c
Karam <Karam.arora20@st.niituniversity.in>
2023-05-03 23:16:35 +0530

    api created

--- server-side/node-mssql-crud/src/routes/warden.routes.js
+++ server-side/node-mssql-crud/src/routes/warden.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { 
-    gatepassApproveOrReject, gatepassCancelAndReject,rejectGatepass, getApprovedGatepass, approveGatepass } from "../controllers/warden.controller"
+    getPendingGatepass,gatepassApproveOrReject, gatepassCancelAndReject,rejectGatepass, getApprovedGatepass, approveGatepass } from "../controllers/warden.controller"
 
 
 
@@ -10,6 +10,7 @@ router.get("/warden/gatepass_approve_or_reject/:id/:flag", gatepassApproveOrReje
 router.put("/warden/approve_gatepass", approveGatepass);
 router.put('/warden/reject_Gatepass', rejectGatepass) // requires gatepass req_id and comments from the client
 router.get('/warden/cancelled_and_rejected_gatepass', gatepassCancelAndReject);
-router.get("/warden/getApprovedGatepass", getApprovedGatepass);
+router.get("/warden/get_Approved_Gatepass", getApprovedGatepass);
+router.get("/warden/get_pending_Gatepass",   getPendingGatepass);
 
 export default router;
