88c11b4cc61a527f3b3c97680bb363e2355ab324
ayushSinha16 <ayush16.sinha@gmail.com>
2023-05-04 21:56:36 +0530

    Cleaned code and removed unnecessary imports

--- client-side/src/Components/wardenComponent/OtherDashboardGatepasses/OtherGatepass.js
+++ client-side/src/Components/wardenComponent/OtherDashboardGatepasses/OtherGatepass.js
@@ -1,11 +1,4 @@
 import "../LatestGatepasses/table.scss";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
 import React, { useEffect, useState } from "react";
 import Cookies from "js-cookie";
 import moment from "moment";
