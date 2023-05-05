6f981acdf929f64582564adc479db618ec229b06
Karam <Karam.arora20@st.niituniversity.in>
2023-05-05 13:46:18 +0530

    req changes PR- 98

--- server-side/node-mssql-crud/src/routes/warden.routes.js
+++ server-side/node-mssql-crud/src/routes/warden.routes.js
@@ -10,9 +10,8 @@ router.get("/warden/gatepass_approve_or_reject/:id/:flag", gatepassApproveOrReje
 router.put("/warden/approve_gatepass", approveGatepass);
 router.put('/warden/reject_Gatepass', rejectGatepass) // requires gatepass req_id and comments from the client
 router.get('/warden/cancelled_and_rejected_gatepass', gatepassCancelAndReject);
-router.get("/warden/get_Approved_Gatepass", getApprovedGatepass);
-router.get("/warden/get_pending_Gatepass",   getPendingGatepass);
-router.get("/warden/getApprovedGatepass", getApprovedGatepass);
+router.get("/warden/get_approved_gatepass", getApprovedGatepass);
+router.get("/warden/get_pending_gatepass",   getPendingGatepass);
 router.get("/warden/get_dashboard_others", getDashboardOthers);
 router.get("/warden/auto_approved_batches",getAutoApprovedBatches);
 
