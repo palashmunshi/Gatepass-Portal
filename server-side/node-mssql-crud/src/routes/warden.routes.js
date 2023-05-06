c74775cb07769fbbdb0f2ccb4f1bcbf452f29a78
ayushSinha16 <ayush16.sinha@gmail.com>
2023-05-06 23:30:26 +0530

    Routes updated for the controller function

--- server-side/node-mssql-crud/src/routes/warden.routes.js
+++ server-side/node-mssql-crud/src/routes/warden.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { 
-    getPendingGatepass,gatepassApproveOrReject, gatepassCancelAndReject,rejectGatepass, getApprovedGatepass, approveGatepass, getDashboardOthers, getAutoApprovedBatches, getDashboardMy } from "../controllers/warden.controller"
+    getPendingGatepass,gatepassApproveOrReject, rejectGatepass, getApprovedGatepass, approveGatepass, getDashboardOthers, getAutoApprovedBatches, getDashboardMy, getAllGatepasses } from "../controllers/warden.controller"
 
 
 
@@ -9,8 +9,7 @@ const router = Router();
 router.get("/warden/gatepass_approve_or_reject/:id/:flag", gatepassApproveOrReject);
 router.put("/warden/approve_gatepass", approveGatepass);
 router.put('/warden/reject_Gatepass', rejectGatepass) // requires gatepass req_id and comments from the client
-router.get('/warden/cancelled_and_rejected_gatepass', gatepassCancelAndReject);
-router.get("/warden/get_approved_gatepass", getApprovedGatepass);
+router.get('/warden/get_all_gatepass', getAllGatepasses);
 router.get("/warden/get_dashboard_my", getDashboardMy);
 router.get("/warden/get_dashboard_others", getDashboardOthers);
 router.get("/warden/auto_approved_batches",getAutoApprovedBatches);
