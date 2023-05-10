5dd27e333175483456dbcb99c49651fa08c75f88
ayushSinha16 <ayush16.sinha@gmail.com>
2023-05-10 17:51:10 +0530

    commit

--- client-side/src/Components/studentComponent/AllGatepasses/GatepassesTable/Gatepasses.js
+++ client-side/src/Components/studentComponent/AllGatepasses/GatepassesTable/Gatepasses.js
@@ -28,29 +28,29 @@ const Gatepasses = () => {
   return (
 
       <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
-        <div className="overflow-hidden">
+        <div className="overflow-hidden shadow-sm sm:rounded-lg">
           <table className="table-auto min-w-full border text-left text-base font-light ">
-            <thead className="border-b  bg-neutral-100 font-medium dark:border-neutral-500">
+            <thead className="border-b  bg-neutral-100 font-medium text-sm dark:border-neutral-500">
               <tr>
-                <th scope="col" className="px-6 py-3">
+                <th scope="col" className="px-3 py-3">
                   Gatepass Type
                 </th>
-                <th scope="col" className="px-6 py-3">
+                <th scope="col" className="px-3 py-3">
                   Departure (Actual)
                 </th>
-                <th scope="col" className="px-6 py-3">
+                <th scope="col" className="px-3 py-3">
                   Arrival (Actual)
                 </th>
-                <th scope="col" className="px-6 py-3">
-                  Requested to
+                <th scope="col" className="px-3 py-3">
+                  Requested To
                 </th>
-                <th scope="col" className="px-6 py-3">
-                  Reason to go
+                <th scope="col" className="px-3 py-3">
+                  Purpose
                 </th>
-                <th scope="col" className="px-6 py-3">
+                <th scope="col" className="px-3 py-3">
                   Status
                 </th>
-                <th scope="col" className="px-6 py-3">
+                <th scope="col" className="px-3 py-3">
                   Action
                 </th>
               </tr>
@@ -61,24 +61,24 @@ const Gatepasses = () => {
                   key={row.request_id}
                   className="rounded-full border-b transition duration-300 ease-in-out hover:bg-neutral-200 dark:border-neutral-500"
                 >
-                  <td className="whitespace-nowrap px-6 py-2 font-normal">
-                    {row.name}
+                  <td className="whitespace-nowrap px-6 py-2 font-normal text-sm leading-5">
+                    {row.gatepass_name}
                   </td>
-                  <td className="whitespace-nowrap px-6 py-2 font-normal">
+                  <td className="whitespace-nowrap px-6 py-2 font-normal text-sm leading-5">
                     {moment(row.from_date).utc().format("DD-MM-YYYY")} |{" "}
                     {moment(row.from_time).utc().format("hh:mm:ss A")}
                   </td>
-                  <td className="whitespace-nowrap px-6 py-2 font-normal">
+                  <td className="whitespace-nowrap px-6 py-2 font-normal text-sm leading-5">
                     {moment(row.to_date).utc().format("DD-MM-YYYY")} |{" "}
                     {moment(row.to_time).utc().format("hh:mm:ss A")}
                   </td>
-                  <td className="whitespace-nowrap px-6 py-2 font-normal">
-                    {row.send_approval_to}
+                  <td className="whitespace-nowrap px-6 py-2 font-normal text-sm leading-5">
+                    {row.requested_to}
                   </td>
-                  <td className="whitespace-nowrap px-6 py-2 font-normal">
+                  <td className="whitespace-nowrap px-6 py-2 font-normal text-sm leading-5">
                     {row.purpose}
                   </td>
-                  <td className="whitespace-nowrap px-6 py-2 font-normal">
+                  <td className="whitespace-nowrap px-6 py-2 font-normal text-sm leading-5">
                     {row.status === "Approved" ? (
                       <span className="text-lime-500">{row.status}</span>
                     ) : row.status === "Rejected" ? (
@@ -91,7 +91,7 @@ const Gatepasses = () => {
                         <span className="text-yellow-400 ">{row.status}</span> 
                     )}
                   </td>
-                  <td className="whitespace-nowrap px-6 py-2 text-center">
+                  <td className="whitespace-nowrap px-6 py-2 text-center font-normal text-sm">
                     <button
                       className="bg-[#1A85BD] hover:bg-sky-800 text-white font-bold py-2 px-4 border border-sky-800 rounded"
                       name={row.request_id}
